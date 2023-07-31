caa7466b655b1f41a808d6c2245b9971235806d4
tienshaoku <tienshaoku@gmail.com>
2023-07-31 21:18:49 +0800

    # First commit: get erc20 balances of a user.

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use ethers::{
+    prelude::abigen,
+    providers::Provider,
+    types::{Address, U256}, utils::{format_units},
+};
+use std::sync::Arc;
+use thousands::Separable;
+
+abigen!(
+    IERC20,
+    r#"[function balanceOf(address account) public view virtual returns (uint256)
+    function decimals() public view virtual returns (uint8)]"#
+);
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    let rpc_url = "https://eth.llamarpc.com";
+    let provider = Arc::new(Provider::try_from(rpc_url)?);
+
+    let erc20_addresses_raw: [&str; 2] = ["0xdAC17F958D2ee523a2206206994597C13D831ec7", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"];
+    let mut erc20_addresses: Vec<Address> = Vec::new();
+    for addr in erc20_addresses_raw {
+        erc20_addresses.push(addr.parse()?)
+    }
+
+    for erc20_addr in erc20_addresses {
+        let cloned_provider = provider.clone();
+        // Initialize a new instance of ERC20   
+        let erc20 = IERC20::new(erc20_addr, cloned_provider);
+
+        let user: Address = "0x57891966931Eb4Bb6FB81430E6cE0A03AAbDe063".parse()?;
+        let balance: U256 =
+        erc20.balance_of(user).call().await?;
+
+        let decimals = erc20.decimals().call().await? as i32;
+
+        println!("the {}(erc20) balance_of {}(user): {:?}", erc20_addr, user, format_units(balance, decimals).unwrap().separate_with_commas());
+    }
+
+    Ok(())
+}
