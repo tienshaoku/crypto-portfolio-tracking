623e46cac41e54cfdfcdbb48f4e2c3aa5c179df6
tienshaoku <tienshaoku@gmail.com>
2023-08-04 16:49:20 +0800

    # Multiple tracking addresses added.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use ethers::{
     types::{Address, U256},
     utils::format_units,
 };
+use std::error::Error;
 use std::sync::Arc;
 use thousands::Separable;
 
@@ -21,35 +22,47 @@ async fn main() -> eyre::Result<()> {
     let rpc_url = "https://eth.llamarpc.com";
     let provider = Arc::new(Provider::try_from(rpc_url)?);
 
-    let erc20_addresses_raw: [&str; 2] = [
-        "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    let tracked_addresses_raw: Vec<&str> = vec![
         "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        "0x57891966931Eb4Bb6FB81430E6cE0A03AAbDe063",
     ];
-    let mut erc20_addresses: Vec<Address> = Vec::new();
-    for addr in erc20_addresses_raw {
-        erc20_addresses.push(addr.parse()?)
-    }
+    let tracked_addresses: Vec<Address> = format_raw_addresses(tracked_addresses_raw).unwrap();
 
-    for erc20_addr in erc20_addresses {
-        let cloned_provider = provider.clone();
-        // Initialize a new instance of ERC20
-        let erc20 = IERC20::new(erc20_addr, cloned_provider);
+    for tracked_addr in tracked_addresses {
+        println!("{:?}", tracked_addr);
 
-        let user: Address = "0x57891966931Eb4Bb6FB81430E6cE0A03AAbDe063".parse()?;
-        let balance: U256 = erc20.balance_of(user).call().await?;
+        let erc20_addresses_raw: Vec<&str> = vec![
+            "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+            "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        ];
+        let erc20_addresses: Vec<Address> = format_raw_addresses(erc20_addresses_raw).unwrap();
 
-        let decimals = erc20.decimals().call().await? as i32;
-        let symbol: String = erc20.symbol().call().await?;
+        for erc20_addr in erc20_addresses {
+            let cloned_provider = provider.clone();
+            // Initialize a new instance of ERC20
+            let erc20 = IERC20::new(erc20_addr, cloned_provider);
 
-        println!(
-            "{} balance_of {}(user): {:?}",
-            symbol,
-            user,
-            format_units(balance, decimals)
-                .unwrap()
-                .separate_with_commas()
-        );
+            let symbol: String = erc20.symbol().call().await?;
+            let balance: U256 = erc20.balance_of(tracked_addr).call().await?;
+            let decimals = erc20.decimals().call().await? as i32;
+
+            println!(
+                "{}: {:?}",
+                symbol,
+                format_units(balance, decimals)
+                    .unwrap()
+                    .separate_with_commas()
+            );
+        }
     }
 
     Ok(())
+}
+
+fn format_raw_addresses(raw_addresses: Vec<&str>) -> Result<Vec<Address>, Box<dyn Error>> {
+    let mut addresses: Vec<Address> = Vec::new();
+    for addr in raw_addresses {
+        addresses.push(addr.parse()?)
+    }
+    Ok(addresses)
 }
