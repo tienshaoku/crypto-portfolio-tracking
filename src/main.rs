b77d9ad1393e22b14e038f8c6afbe5f4c9d44aff
tienshaoku <tienshaoku@gmail.com>
2023-09-28 19:10:31 +0800

    # Renaming.

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,18 @@ abigen!(
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let tracked_addresses: Vec<Address> = format_raw_addresses(&constant::ADDRESS).unwrap();
+    let wallets: Vec<Address> = format_raw_addresses(&constant::ADDRESS).unwrap();
 
-    let mut token_map: HashMap<&str, &[&'static str]> = HashMap::new();
-    token_map.insert(constant::ETH_RPC, &constant::ETH_ERC20);
-    token_map.insert(constant::OP_RPC, &constant::OP_ERC20);
+    let mut rpc_token_map: HashMap<&str, &[&'static str]> = HashMap::new();
+    rpc_token_map.insert(constant::ETH_RPC, &constant::ETH_ERC20);
+    rpc_token_map.insert(constant::OP_RPC, &constant::OP_ERC20);
 
-    for tracked_addr in tracked_addresses {
-        println!("{:?}", tracked_addr);
+    for wallet in wallets {
+        println!("{:?}", wallet);
 
-        for (rpc_url, erc20_addresses_raw) in &token_map {
+        for (rpc_url, erc20_addresses_raw) in &rpc_token_map {
             println!("rpc_url: {}", rpc_url);
-            // dereference once for the pointer on token_map
+            // dereference once for the pointer on rpc_token_map
             let provider = Arc::new(Provider::try_from(*rpc_url)?);
 
             let erc20_addresses: Vec<Address> = format_raw_addresses(erc20_addresses_raw).unwrap();
@@ -42,7 +42,7 @@ async fn main() -> eyre::Result<()> {
                 let erc20 = IERC20::new(erc20_addr, cloned_provider);
 
                 let symbol: String = erc20.symbol().call().await?;
-                let balance: U256 = erc20.balance_of(tracked_addr).call().await?;
+                let balance: U256 = erc20.balance_of(wallet).call().await?;
                 let decimals = erc20.decimals().call().await? as i32;
 
                 println!(
